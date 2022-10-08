77612d64bfbaa04c9e6e680f15186a4fd0949a3f
Julio Sánchez <jsanchez@innomius.com>
2022-10-08 13:10:24 -0500

    first commit

---
+++ home.js
@@ -0,0 +1,15 @@
+function hoverJuega(element) {
+  element.setAttribute('src', './public/home/01_juega_on.png');
+}
+
+function unhoverJuega(element) {
+  element.setAttribute('src', './public/home/01_juega.png');
+}
+
+function hoverConsulta(element) {
+  element.setAttribute('src', './public/home/01_consulta_on.png');
+}
+
+function unhoverConsulta(element) {
+  element.setAttribute('src', './public/home/01_consulta.png');
+}
