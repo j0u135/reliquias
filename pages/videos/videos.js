dc0ff4102f4ad3b73523af493aa6e2dc2c3b93ae
Julio Sánchez <j0u135.uam@gmail.com>
2022-10-09 00:27:49 -0500

    Videos y Recursos

---
+++ pages/videos/videos.js
@@ -0,0 +1,48 @@
+function hoverninos(element) {
+  element.setAttribute('src', '../../public/videos/04_ninos_on.png');
+}
+
+function unhoverninos(element) {
+  element.setAttribute('src', '../../public/videos/04_ninos.png');
+}
+
+function hoverjovenes(element) {
+  element.setAttribute('src', '../../public/videos/04_jovenes_on.png');
+}
+
+function unhoverjovenes(element) {
+  element.setAttribute('src', '../../public/videos/04_jovenes.png');
+}
+
+function hoveradultos(element) {
+  element.setAttribute('src', '../../public/videos/04_adultos_on.png');
+}
+
+function unhoveradultos(element) {
+  element.setAttribute('src', '../../public/videos/04_adultos.png');
+}
+
+function hoveradultosm(element) {
+  element.setAttribute('src', '../../public/videos/04_mayores_on.png');
+}
+
+function unhoveradultosm(element) {
+  element.setAttribute('src', '../../public/videos/04_mayores.png');
+}
+
+function hovermexext(element) {
+  element.setAttribute('src', '../../public/videos/04_ext_on.png');
+}
+
+function unhovermexext(element) {
+  element.setAttribute('src', '../../public/videos/04_ext.png');
+}
+
+function hoverregresar(element) {
+  element.setAttribute('src', '../../public/videos/04_regresar_on.png');
+}
+
+function unhoverregresar(element) {
+  element.setAttribute('src', '../../public/videos/04_regresar.png');
+}
+
