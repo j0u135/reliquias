f9ca8906d19091e7a1dd2b256a2cd59913f7bb1a
Julio Sánchez <j0u135.uam@gmail.com>
2022-10-09 15:51:29 -0500

    juego

---
+++ pages/juego/juego.js
@@ -0,0 +1,15 @@
+function hoverInfografia(element) {
+  element.setAttribute('src', '../../public/juego/02_adultos_on.png');
+}
+
+function unhoverInfografia(element) {
+  element.setAttribute('src', '../../public/juego/02_adultos.png');
+}
+
+function hoverVideo(element) {
+  element.setAttribute('src', '../../public/juego/02_ninos_on.png');
+}
+
+function unhoverVideo(element) {
+  element.setAttribute('src', '../../public/juego/02_ninos.png');
+}
