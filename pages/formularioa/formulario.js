345f796bad8e4dbf8f406bfb4c058a8aa1cce8f0
Julio Sánchez <j0u135.uam@gmail.com>
2022-10-09 16:23:30 -0500

    Juego y formulario

---
+++ pages/formularioa/formulario.js
@@ -0,0 +1,7 @@
+function hoverentrar(element) {
+  element.setAttribute('src', '../../public/formulario/F_entrar_on.png');
+}
+
+function unhoverentrar(element) {
+  element.setAttribute('src', '../../public/formulario/F_entrar.png');
+}
---
+++ pages/zonajuegon/zonajuego.js
@@ -0,0 +1,7 @@
+function hoversalir(element) {
+  element.setAttribute('src', '../../public/zonajuego/J_B_salir_ON.png');
+}
+
+function unhoversalir(element) {
+  element.setAttribute('src', '../../public/zonajuego/J_B_salir.png');
+}
